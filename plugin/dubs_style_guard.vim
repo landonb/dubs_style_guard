landonb/dubs_style_guard

commit 63411b2d39f133c6666e960358ec748274bdd524

fix: avoid matchbufline on Vim 8 (disable modeline grep sussers)
This plugin doesn't technically support Vim <9, but I still
have an old host running Vim 8. So while this fix won't make
the plugin continue to work *well* on Vim 8, it'll at least
still run without an error (but it won't find the modeline).

plugin/dubs_style_guard.vim
@@ -600,6 +600,12 @@ function s:ExtractModelineCmdsFromBufferHeadOrTail() abort
 endfunction
 
 function s:ExtractModelineCmdsFromBuffer(lnum, end) abort
+  if v:version < 900
+    " No matchbufline
+
+    return ''
+  endif
+
   let l:extracted_cmds = ''
 
   " Note that the captured input associated with a matchbufline "submatches"
@@ -659,6 +665,12 @@ endfunction
 "     - In any case, lesson learned: ** Avoid system() calls. **
 
 function! s:CountFileTabsAndSpaces() abort
+  if v:version < 900
+    " No matchbufline
+
+    return [0, 0]
+  endif
+
   let l:pattern_spaced = '^ '
   let l:pattern_tabbed = '^\t'
 

commit 5ce0659d105185053ec9fb89d81a96f24e3dc762

fix: repair nvim regression from 2 commits back
Fixes: 63411b2 ∙ fix: avoid matchbufline on Vim 8 (disable modeline grep sussers)

plugin/dubs_style_guard.vim
@@ -605,7 +605,7 @@ function s:ExtractModelineCmdsFromBufferHeadOrTail() abort
 endfunction
 
 function s:ExtractModelineCmdsFromBuffer(lnum, end) abort
-  if v:version < 900
+  if !has('nvim') && v:version < 900
     " No matchbufline
 
     return ''
@@ -670,7 +670,7 @@ endfunction
 "     - In any case, lesson learned: ** Avoid system() calls. **
 
 function! s:CountFileTabsAndSpaces() abort
-  if v:version < 900
+  if !has('nvim') && v:version < 900
     " No matchbufline
 
     return [0, 0]